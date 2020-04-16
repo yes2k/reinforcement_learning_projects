701a6dc0ff6efc4eeda32a1b40c613dbf3e2180a
Yatharth Khattar <yatharthkhattar@gmail.com>
2020-04-16 15:37:21 -0400

    Added way to learnbased on last n steps for cartpole environments

--- testing/gym_test.py
+++ testing/gym_test.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import random
 import matplotlib.pyplot as plt
+from itertools import chain
 
 class DQL():
     def __init__(self, state_dim, hidden_dim, action_dim, lr):
@@ -31,21 +32,31 @@ class DQL():
             return self.model(torch.Tensor(state))
 
 
-def q_learning(env, model, episodes, gamma=0.9,
+def q_learning(env, model, episodes, num_past_states, gamma=0.9,
                epsilon=0.3, eps_decay=0.99):
     final = []
     for episode in range(episodes):
         print(episode)
-        last_4_states = []
+
+        last_n_states = []
+
         state = env.reset()
+        last_n_states.append(state)
+
+        # if there aren't enough past states, we pad them with zeros
+        while len(last_n_states) < num_past_states:
+            last_n_states.append([0 for i in range(len(last_n_states[0]))])
 
         done = False
         total = 0
         while not done:
+            curr_last_n_states = list(chain.from_iterable(last_n_states))
+            print(last_n_states)
+            # Choosing random action with probability epsilon (epsilon greedy)
             if random.random() < epsilon:
                 action = env.action_space.sample()
             else:
-                q_values = model.predict(state)
+                q_values = model.predict(curr_last_n_states)
                 action = torch.argmax(q_values).item()
 
             next_state, reward, done, _ = env.step(action)
@@ -53,24 +64,30 @@ def q_learning(env, model, episodes, gamma=0.9,
 
             total += reward
 
-            q_values = model.predict(state).tolist()
+            q_values = model.predict(curr_last_n_states).tolist()
 
             if done:
                 q_values[action] = reward
-                model.update(state, q_values)
+                model.update(curr_last_n_states, q_values)
             else:
-                q_values_next = model.predict(next_state)
+                last_n_states.pop()
+                last_n_states.insert(0, next_state)
+                q_values_next = model.predict(list(chain.from_iterable(last_n_states)))
                 q_values[action] = reward + gamma * torch.max(q_values_next).item()
-                model.update(state, q_values)
-            state = next_state
+                model.update(curr_last_n_states, q_values)
+            # state = next_state
         epsilon = max(epsilon * eps_decay, 0.01)
         final.append(total)
     return final
 
 
 env = gym.envs.make("CartPole-v1")
+
+# Number of past states to input into our NN
+n_past_states = 1
+
 # Number of states
-n_state = env.observation_space.shape[0]
+n_state = env.observation_space.shape[0]*n_past_states
 
 # Number of actions
 n_action = env.action_space.n
@@ -84,7 +101,8 @@ n_hidden = 50
 lr = 0.001
 
 simple_dqn = DQL(n_state, n_hidden, n_action, lr)
-simple = q_learning(env, simple_dqn, episodes, gamma=.9, epsilon=0.3)
+simple = q_learning(env, simple_dqn, episodes, n_past_states,
+                gamma=.9, epsilon=0.3)
 
 plt.figure()
 plt.plot(range(episodes), simple)
